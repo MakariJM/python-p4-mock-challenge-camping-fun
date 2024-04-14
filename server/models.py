6c943784e78632e8d213cc0f8dbd3e573c912c6b
JuliusMakari <muragejmakari@gmail.com>
2024-04-14 18:26:48 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -24,10 +24,10 @@ class Activity(db.Model, SerializerMixin):
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
 
-    # Add relationship
-    
-    # Add serialization rules
-    
+    signups = relationship('Signup', back_populates='activity', cascade='all, delete-orphan')
+
+    serialize_rules = ('-signups.activity',)
+
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
 
@@ -39,13 +39,22 @@ class Camper(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
 
-    # Add relationship
-    
-    # Add serialization rules
-    
-    # Add validation
-    
-    
+    signups = relationship('Signup', back_populates='camper', cascade='all, delete-orphan')
+
+    serialize_rules = ('-signups.camper',)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name is required.")
+        return name
+
+    @validates('age')
+    def validate_age(self, key, age):
+        if age < 8 or age > 18:
+            raise ValueError("Age must be between 8 and 18.")
+        return age
+
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
 
@@ -56,14 +65,19 @@ class Signup(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
 
-    # Add relationships
-    
-    # Add serialization rules
-    
-    # Add validation
-    
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id', ondelete='CASCADE'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id', ondelete='CASCADE'))
+
+    camper = relationship('Camper', back_populates='signups')
+    activity = relationship('Activity', back_populates='signups')
+
+    serialize_rules = ('-camper.signups', '-activity.signups',)
+
+    @validates('time')
+    def validate_time(self, key, time):
+        if time < 0 or time > 23:
+            raise ValueError("Time must be between 0 and 23.")
+        return time
+
     def __repr__(self):
         return f'<Signup {self.id}>'
-
-
-# add any models you may need.
